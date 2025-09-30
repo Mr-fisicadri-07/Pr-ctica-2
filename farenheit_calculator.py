ab7b9e2f1123c5fdd0688231a390c92132cda5a9
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-09-30 11:46:05 +0200

    Hardcoding fixed
    No more hardcoding

--- farenheit_calculator.py
+++ farenheit_calculator.py
@@ -1,12 +1,16 @@
 # this is a Farenheit degree calculator
 
-temperature_celsius = 100
+temperature_celsius = input("indica la temperatura en grados celsius: ")
+
+temperature_celsius = int(temperature_celsius)
 
 temperature_farenheit = (temperature_celsius * 9/5) + 32
 
 print(temperature_farenheit)
 
-temperature_kelvin = 373.15
+temperature_kelvin = input("indica la temperatura en grados kelvin: ")
+
+temperature_kelvin = int(temperature_kelvin)
 
 temperature_farenheit = (temperature_kelvin - 273.15) * 9/5 + 32
 
