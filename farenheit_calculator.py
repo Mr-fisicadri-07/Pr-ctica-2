bf61d45ae16f8300ab796f15ea9f6b14ebd63f89
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-09-30 12:01:33 +0200

    Fixced
    Fixed it again

--- farenheit_calculator.py
+++ farenheit_calculator.py
@@ -1,16 +1,12 @@
 # this is a Farenheit degree calculator
 
-temperature_celsius = input("indica la temperatura en grados celsius: ")
-
-temperature_celsius = int(temperature_celsius)
+temperature_celsius = float(input("indica la temperatura en grados celsius: "))
 
 temperature_farenheit = (temperature_celsius * 9/5) + 32
 
 print(temperature_farenheit)
 
-temperature_kelvin = input("indica la temperatura en grados kelvin: ")
-
-temperature_kelvin = int(temperature_kelvin)
+temperature_kelvin = float(input("indica la temperatura en grados kelvin: "))
 
 temperature_farenheit = (temperature_kelvin - 273.15) * 9/5 + 32
 
---
+++ kinetic_energy.py
@@ -0,0 +1,9 @@
+#this is a calculator of kinetic energy
+
+mass = float(input("indica la masa en kg: "))
+
+velocity = float(input("indica la velocidad en m/s: "))
+
+kinetic_energy = (0.5 * mass * velocity ** 2)
+
+print(kinetic_energy)
