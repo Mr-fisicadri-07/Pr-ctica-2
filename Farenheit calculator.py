c677617719e6f509326f6cd44a4476a8b4689e12
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-09-30 11:33:49 +0200

    Debug
    I have fixed the calculator

--- Farenheit calculator.py
+++
@@ -1,5 +0,0 @@
-# this is a Farenheit degree calculator
-
-celsius = ("x")
-
-farenheit = (celsius * 9/5) + 32
---
+++ farenheit_calculator.py
@@ -0,0 +1,13 @@
+# this is a Farenheit degree calculator
+
+temperature_celsius = 100
+
+temperature_farenheit = (temperature_celsius * 9/5) + 32
+
+print(temperature_farenheit)
+
+temperature_kelvin = 373.15
+
+temperature_farenheit = (temperature_kelvin - 273.15) * 9/5 + 32
+
+print(temperature_farenheit)
