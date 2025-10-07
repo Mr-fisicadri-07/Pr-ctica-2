d1ca32b7a41b3ff8140858cb3770fcbccd18eeaf
Mr-fisicadri-07 <adrianlof07@gmail.com>
2025-10-07 11:50:05 +0200

    Debugging
    Forgot about time XD

--- object_motions_calculator.py
+++ object_motions_calculator.py
@@ -8,9 +8,11 @@ velocity_x = float(input("indica la velocidad en x (m/s): "))
 
 aceleration_x = float(input("indica la aceleración en x (m/s²): "))
 
-position_x_end = position_x + velocity_x + (0.5 * aceleration_x)
+time = float(input("indica el tiempo (s): "))
 
-velocity_x_end = velocity_x + aceleration_x
+position_x_end = position_x + velocity_x*time + (0.5 * aceleration_x*time**2)
+
+velocity_x_end = velocity_x + aceleration_x*time
 
 output_x = f"La posición en x es de {position_x_end} metros y la velocidad en x es de {velocity_x_end} m/s"
 
